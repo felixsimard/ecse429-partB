7adf1d233e372d4c9ead4b228e0da3c32bdb9d5f
CharlesBourbeau <bourbeau.charles99@gmail.com>
2020-11-11 11:48:16 -0500

    Add scenarios to story01

--- ecse429-storytesting/src/test/java/ecse429/storytesting/StepDefinitions.java
+++ ecse429-storytesting/src/test/java/ecse429/storytesting/StepDefinitions.java
@@ -2,6 +2,7 @@ package ecse429.storytesting;
 
 import cucumber.api.DataTable;
 import cucumber.api.java.en.*;
+import cucumber.api.PendingException;
 
 import java.util.List;
 import java.util.Map;
@@ -9,16 +10,52 @@ import java.util.Map;
 public class StepDefinitions {
 
     @Given("a task containing")
-    public void step_def(DataTable table) {
+    public void a_task_containing(DataTable table) {
         /* extract to-do's data */
-        Map<String, String> row = table.asMap(String.class, String.class);
-        String title = row.get("title");
-        boolean completed = Boolean.parseBoolean(row.get("completed"));
-        boolean active = Boolean.parseBoolean(row.get("active"));
-        String description = row.get("description");
+        List<Map<String, String>> list = table.asMaps(String.class, String.class);
+        String title = list.get(0).get("title");
+        boolean completed = Boolean.parseBoolean(list.get(0).get("completed"));
+        boolean active = Boolean.parseBoolean(list.get(0).get("active"));
+        String description = list.get(0).get("description");
 
         /* create the to-do */
         HelperFunctions.createTodo(title, completed, active, description);
+    }
+
+    @Given("^a category containing$")
+    public void a_category_containing(DataTable arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
+
+    @When("^I link the above task to the (.*) priority category$")
+    public void i_link_the_above_task_to_the_priority_category(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the task is categorized with the corresponding priority$")
+    public void the_task_is_categorized_with_the_corresponding_priority() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^multiple categories containing$")
+    public void multiple_categories_containing(DataTable arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+        // E,K,V must be a scalar (String, Integer, Date, enum etc)
+        throw new PendingException();
+    }
+
+    @Given("^a link between the above task and a high priority category$")
+    public void a_link_between_the_above_task_and_a_high_priority_category() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 
 }
--- ecse429-storytesting/src/test/resources/ecse429/storytesting/Story01.feature
+++ ecse429-storytesting/src/test/resources/ecse429/storytesting/Story01.feature
@@ -4,15 +4,36 @@ Feature: Task priority categorization
     so I can better manage my time.
 
     # Normal Flow
-    Scenario Outline: Categorize task as HIGH priority
+    Scenario: Categorize task as HIGH priority
         Given a task containing
             | title            | completed | active | description              |
             | ECSE 429 part B  | false     | true   | Complete individual part |
         And a category containing
             | title   | description    |
             | HIGH    | High priority  |
-        When I link the above task to the above category
+        When I link the above task to the <level> priority category
         Then the task is categorized with the corresponding priority
 
-        Examples:
-            |  |
+    # Alternate Flow
+    Scenario: Change the category of a task from HIGH to MEDIUM
+        Given a task containing
+            | title            | completed | active | description              |
+            | ECSE 429 part B  | false     | true   | Complete individual part |
+        And multiple categories containing
+            | title   | description     |
+            | HIGH    | High priority   |
+            | MEDIUM  | Medium priority |
+        And a link between the above task and a high priority category
+        When I link the above task to the <level> priority category
+        Then the task is categorized with the corresponding priority
+
+    # Error flow
+    Scenario: Categorize task with non existent category
+        Given a task containing
+            | title            | completed | active | description              |
+            | ECSE 429 part B  | false     | true   | Complete individual part |
+        And a category containing
+            | title        | description         |
+            | null         | null                |
+        When I link the above task to the <level> priority category
+        Then the task is categorized with the corresponding priority
