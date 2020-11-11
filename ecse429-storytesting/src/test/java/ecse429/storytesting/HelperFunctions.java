7f7c14c0785a5625691aa0cea5e1dcbf3b51bd94
Tyler Watson <tylerjrwatson@gmail.com>
2020-11-11 13:43:44 -0500

    for charlie <3

--- ecse429-storytesting/src/test/java/ecse429/storytesting/HelperFunctions.java
+++ ecse429-storytesting/src/test/java/ecse429/storytesting/HelperFunctions.java
@@ -1,5 +1,8 @@
 package ecse429.storytesting;
 
+import ecse429.storytesting.Model.Project;
+import ecse429.storytesting.Model.Todo;
+import gherkin.deps.com.google.gson.Gson;
 import org.json.simple.JSONObject;
 
 import io.restassured.RestAssured;
@@ -10,8 +13,14 @@ import io.restassured.specification.RequestSpecification;
 public class HelperFunctions {
 
     private static final int STATUS_CODE_CREATED = 201;
+    private static Gson gson = new Gson();
 
-    public static int createTodo(String title, boolean completed, boolean active, String description) {
+
+    public HelperFunctions() {
+        RestAssured.baseURI = "http://localhost:4567/";
+    }
+
+    public static Project createProject(String title, boolean completed, boolean active, String description) {
         RequestSpecification request = RestAssured.given();
 
         JSONObject requestParams = new JSONObject();
@@ -23,10 +32,27 @@ public class HelperFunctions {
 
         request.body(requestParams.toJSONString());
 
+        Response response = request.post("/projects");
+
+        Project result = gson.fromJson(response.asString(), Project.class);
+        return result;
+    }
+
+    public static Todo createTodo(String title, boolean doneStatus, String description) {
+        RequestSpecification request = RestAssured.given();
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("title", title);
+        requestParams.put("doneStatus", doneStatus);
+        requestParams.put("description", description);
+
+
+        request.body(requestParams.toJSONString());
+
         Response response = request.post("/todos");
 
-        int todoID = Integer.parseInt((String) response.jsonPath().get("id"));
-        return todoID;
+        Todo result = gson.fromJson(response.asString(), Todo.class);
+        return result;
     }
 
 }
---
+++ ecse429-storytesting/src/test/java/ecse429/storytesting/Model/Project.java
@@ -0,0 +1,9 @@
+package ecse429.storytesting.Model;
+
+public class Project {
+    public int id;
+    public String title;
+    public boolean completed;
+    public boolean active;
+    public String description;
+}
